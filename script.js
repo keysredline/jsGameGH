7626e5067c8252e839c1b14f02d9ed384adb8495
KEYS <jf0934599@otc.edu>
2021-02-28 19:11:02 -0800

    sunday fun day

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-//DEV Notes: btn inner html able to see charG item src
-//need to assign a dict or way to attach 3 values to the charG item
-// link up charG index to a var that points to a key
-//map keys in ES6 cannot hold multiple values...wtf
-//on page load, call anonymous func
+//DEV Notes: left off trying to link up counter
+
 window.addEventListener('load',function(){
-    var timerThread = setInterval(imageShuffle,1000)
+    setInterval(imageShuffle,1000)
     //setting global vars, Arrays, Map
     counter = 0;
     fillerCard=["img/cc.png","img/cc1.png","img/cc2.png","img/cc3.png",
@@ -25,33 +22,48 @@ window.addEventListener('load',function(){
     coll.set(charG[8], "Saber");
 
     fillerImg = document.getElementsByClassName('img');
-    imageShuffle()
+    //imageShuffle()
     scoreBoard= document.getElementById('score');
     score=0;
-    
+    reactionBoard= document.getElementById('reac');
 })
 
 //func sets up filler images taken from Arr
 //while cycling them through the game board
 //timeout method was used to delay changes
 //.random method used to generate random int's
+
+// function imageShuffle()
+// {
+//     setTimeout(function(){
+//     
+//     for(let i =0; i < fillerCard.length; i++)
+//     {
+//         randomIndex=Math.floor(Math.random() * 9);
+//         fillerImg[i].src=fillerCard[randomIndex];
+//         counter++
+//         //console.log(fillerImg[i]);        
+//     }
+//     //limiter: enter below function every 4 sec
+//     if (counter % 4 ==0)
+//     {
+//         guessChar()
+//     }
+//     },500);//time out param
+// }
 function imageShuffle()
 {
-    setTimeout(function(){
-    //queuedImg.removeEventListener('click',btnPop);
     for(let i =0; i < fillerCard.length; i++)
     {
         randomIndex=Math.floor(Math.random() * 9);
         fillerImg[i].src=fillerCard[randomIndex];
-        counter++
-        //console.log(fillerImg[i]);        
+        counter++      
     }
     //limiter: enter below function every 4 sec
     if (counter % 4 ==0)
     {
         guessChar()
     }
-    },500);//time out param
 }
 //func sets up 2 random ints
 //assigns an image from charG Arr to one img block
@@ -62,24 +74,18 @@ function guessChar()
     randomIndex2=Math.floor(Math.random() * 9);
     fillerImg[randomIndex1].src=charG[randomIndex2];
     queuedImg=fillerImg[randomIndex1];
-    
-    //alert('1')
+    //set TO to remove EL 1s after
     setTimeout(function(){
         queuedImg.removeEventListener('click',btnPop);
-        
-        //alert('2')
     },1000)
-    //alert('3')
-    queuedImg.addEventListener('click', btnPop)
-    
-    console.log(queuedImg)
-    //console.log(randomIndex1)
-    //console.log(randomIndex2)
+    queuedImg.addEventListener('click', btnPop);
+
+    timer = Date.now();
+
 }
 
 function btnPop()
 {   
-    //queuedImg.removeEventListener('click',btnPop);
     randomCounter=Math.floor(Math.random() * 3);
     randomIndex3=Math.floor(Math.random() * 9);
     randomIndex4=Math.floor(Math.random() * 9);
@@ -87,67 +93,94 @@ function btnPop()
     {
         case 0:
             button = document.createElement('button');
+            button.className = 'answerButton';
             button.innerHTML = coll.get(charG[randomIndex2]);
             document.getElementById('btn').appendChild(button);
             button.addEventListener('click', answerRight);
             
-            button1 = document.createElement('button');            
+            button1 = document.createElement('button');
+            button1.className = 'answerButton';            
             button1.innerHTML = wrongChoice[randomIndex3];
             document.getElementById('btn1').appendChild(button1);
             button1.addEventListener('click', answerWrong);
 
             button2 = document.createElement('button');
+            button2.className = 'answerButton';
             button2.innerHTML = wrongChoice[randomIndex4];
             document.getElementById('btn2').appendChild(button2);
             button2.addEventListener('click', answerWrong);
-            console.log(randomCounter)
+            
+            setTimeout(function(){
+                button.remove();
+                button1.remove();
+                button2.remove();
+            },1000)
             break;
         case 1:
             button = document.createElement('button');
+            button.className = 'answerButton';
             button.innerHTML = coll.get(charG[randomIndex2]);
             document.getElementById('btn1').appendChild(button);
             button.addEventListener('click', answerRight);
             
-            button1 = document.createElement('button');            
+            button1 = document.createElement('button');
+            button1.className = 'answerButton';            
             button1.innerHTML = wrongChoice[randomIndex3];
             document.getElementById('btn').appendChild(button1);
             button1.addEventListener('click', answerWrong);
 
             button2 = document.createElement('button');
+            button2.className = 'answerButton';
             button2.innerHTML = wrongChoice[randomIndex4];
             document.getElementById('btn2').appendChild(button2);
             button2.addEventListener('click', answerWrong);
-            console.log(randomCounter)
+            
+            setTimeout(function(){
+                button.remove();
+                button1.remove();
+                button2.remove();
+            },1000)
             break;
         case 2:
             button = document.createElement('button');
+            button.className = 'answerButton';
             button.innerHTML = coll.get(charG[randomIndex2]);
             document.getElementById('btn2').appendChild(button);
             button.addEventListener('click', answerRight);
             
-            button1 = document.createElement('button');            
+            button1 = document.createElement('button');
+            button1.className = 'answerButton';            
             button1.innerHTML = wrongChoice[randomIndex3];
             document.getElementById('btn1').appendChild(button1);
             button1.addEventListener('click', answerWrong);
 
             button2 = document.createElement('button');
+            button2.className = 'answerButton';
             button2.innerHTML = wrongChoice[randomIndex4];
             document.getElementById('btn').appendChild(button2);
             button2.addEventListener('click', answerWrong);
-            console.log(randomCounter)
+            
+            setTimeout(function(){
+                button.remove();
+                button1.remove();
+                button2.remove();
+            },1000)
             break;         
-    }
+    }  
 }
 
 function answerRight()
 {
+    reaction = Date.now() - timer;
+    reCalc = reaction-1000;
+    reactionBoard.innerHTML=reCalc;
+    bonus=(1000 - reCalc) * 100
+    // reaction = Date.now() - timer;
+    // reactionBoard.innerHTML=Math.floor(reaction / 100);
     button.removeEventListener('click',answerRight)
     button1.removeEventListener('click',answerWrong)
     button2.removeEventListener('click',answerWrong)
-    button.remove();
-    button1.remove();
-    button2.remove();
-    score+=500;
+    score+=500+bonus;
     scoreBoard.innerHTML= score;
 }
 
@@ -156,11 +189,26 @@ function answerWrong()
     button.removeEventListener('click',answerRight)
     button1.removeEventListener('click',answerWrong)
     button2.removeEventListener('click',answerWrong)
-    button.remove();
-    button1.remove();
-    button2.remove();
     score-=400;
     scoreBoard.innerHTML= score;
 }
 
+
+
+// function updateCount(){
+//     speed=10;
+//     //scoreBoard.innerHTML= score;
+//     target = +score
+//     console.log(target)
+//     count = +scoreBoard.innerHTML
+//     console.log(count)
+//     inc = target / speed;
+    
+//     if (count < target ){
+//         count.innerHTML= count + inc;
+//         setTimeout(updateCount, 1000)
+//     }else{
+//         count.innerHTML=target;
+//     }
+// }
 
